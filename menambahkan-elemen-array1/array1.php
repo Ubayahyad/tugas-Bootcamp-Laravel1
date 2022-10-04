db84537432bd363aff554a90b7f46e9d6d80ca50
Ubayahyad <ahyaduaby@gmail.comgit commitahyad ubaygit config --global user.emailahyadubay@gmail.com>
2022-10-04 20:14:05 +0700

    update code

--- menambahkan-elemen-array1/array1.php
+++ menambahkan-elemen-array1/array1.php
@@ -1,7 +1,10 @@
 <?php
 	$arr = array(1,2,3,4,5);
-	
+	$inc = 0;
+
 	foreach($arr as $value) {
-	  echo "array ke => $value \n";
-	}
+    $inc += $value;
+    echo "array ke => $value <br>";
+	} 
+  echo "jumlah total: $inc";
 ?>
